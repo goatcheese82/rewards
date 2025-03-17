ee0f163a2127b553bf987c90b8f9035daf30b1c9
Ben Smith <nonsquishy@gmail.com>
2025-03-17 07:41:10 -0400

    ?

--- src/components/WeeklyCalendar.vue
+++ src/components/WeeklyCalendar.vue
@@ -104,7 +104,7 @@
             <svg class="icon" viewBox="0 0 24 24">
               <path
                 fill="currentColor"
-                d="M15 16h4v2h-4zm0-8h7v2h-7zm0 4h6v2h-6zM3 18c0 1.1.9 2 2 2h6c1.1 0 2-.9 2-2V8H3v10zM14 5h-3l-1-1H6L5 5H2v2h12z"
+                d="M3 18c0 1.1.9 2 2 2h6c1.1 0 2-.9 2-2V8H3v10zM14 5h-3l-1-1H6L5 5H2v2h12z"
               />
             </svg>
             <div class="checkbox-pair">
@@ -180,7 +180,7 @@
         <svg class="icon" viewBox="0 0 24 24">
           <path
             fill="currentColor"
-            d="M15 16h4v2h-4zm0-8h7v2h-7zm0 4h6v2h-6zM3 18c0 1.1.9 2 2 2h6c1.1 0 2-.9 2-2V8H3v10zM14 5h-3l-1-1H6L5 5H2v2h12z"
+            d="M3 18c0 1.1.9 2 2 2h6c1.1 0 2-.9 2-2V8H3v10zM14 5h-3l-1-1H6L5 5H2v2h12z"
           />
         </svg>
         <span>Cleans up after being asked</span>
